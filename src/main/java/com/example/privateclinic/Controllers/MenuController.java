d2cd6117f5958cd233aa7ca8b77dae58a1789760
AndreNguyen03 <123562712+AndreNguyen03@users.noreply.github.com>
2024-04-22 14:12:00 +0700

    update fxml UI

---
+++ src/main/java/com/example/privateclinic/Controllers/MenuController.java
@@ -0,0 +1,30 @@
+package com.example.privateclinic.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
+public class MenuController {
+
+    @FXML
+    private Button btnAccountant;
+
+    @FXML
+    private Button btnCategory;
+
+    @FXML
+    private Button btnExamination;
+
+    @FXML
+    private Button btnLogout;
+
+    @FXML
+    private Button btnReport;
+
+    @FXML
+    private Button btnSetting;
+
+    @FXML
+    private Text titleTextField;
+
+}
